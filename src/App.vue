d2792a5b82f5daf6094fbb362a1a3725d2781f9c
AayushiShukla7 <AayushiShukla27@gmail.com>
2022-05-01 17:15:36 +0530

    Multiplayer mode implemented

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,92 @@
 <script setup>
+import { ref, computed } from 'vue'
+
+const player = ref('X');  //The player
+const board = ref([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+]); //The game board
+
+//All posible Winning scenarios on the current board
+//Logic from - https://reactjs.org/tutorial/tutorial.html#declaring-a-winner  
+const CalculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+const winner = computed(() => CalculateWinner(board.value.flat()));  //Calculate and declare the winner
+
+//Gameplay
+const MakeMove = (x, y) => {
+  if (winner.value) return;   //Game Over - Winner is declared
+
+  if(board.value[x][y] != "") return;   //The position is already filled. Can't make a move here.
+
+  board.value[x][y] = player.value;   //Player makes a move
+
+  player.value = player.value === 'X' ? 'O' : 'X';    //Switch between players
+};
+
+const ResetGame = () => {
+  board.value = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+
+  player.value = 'X'; //Set the player value back to 'X'
+};
 
 </script>
 
 <template>
-  <h1>Hello, world!</h1>
+  <main class="pt-8 text-center dark:bg-gray-800 min-h-screen dark:text-white">
+    <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+    
+    <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+
+    <div class="flex flex-col items-center mb-8">
+      <div 
+        v-for="(row, x) in board"
+        :key="x"
+        class="flex">
+
+        <div
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="MakeMove(x, y)"
+          :class="`border border-white w-20 h-20 hover:bg-gray-700 flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ${ cell === 'X' ? 'text-red-500' : 'text-green-400' }`">
+          {{ cell === 'X' ? 'close' : cell === 'O' ? 'circle' : '' }}
+        </div>
+
+      </div>
+    </div>
+
+    <h2 v-if="winner" class="text-6xl font-bold mb-8">Player '{{ winner }}' wins!</h2>
+
+    <button @click="ResetGame" class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-600 duration-300">Reset Game</button>
+
+  </main>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
